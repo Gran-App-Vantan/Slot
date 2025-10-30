e6bd2bf668ac59338f609231e284b4c2762213a1
shion <shion48691811@gmail.com>
2025-10-30 17:57:15 +0900

    start/stopのbuttonを作成

---
+++ frontend/slot-app/app/game/page.tsx
@@ -0,0 +1,10 @@
+import StartStop from "@/components/StartStop";
+
+function GamePage() {
+    return <div>
+        <h1 className="">Game Page</h1>
+        <StartStop />
+    </div>
+}
+
+export default GamePage;
