028e1c7ffcb3872b27c27b41fc7ae59b7066fc47
ruka <vanilla.0704@icloud.com>
2025-11-14 22:56:34 +0900

    コントローラー

---
+++ backend/slot_app/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GameController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->prefix('game')->group(function () {
+    Route::post('/create-url', [GameController::class, 'createUrl']);
+});
